0b8b317693a3bc92e2b0aae81a545eaa5f1be363
Uzay Kuzey <atauzayk@gmail.com>
2024-12-22 23:15:27 +0300

    triple update 🥳

--- Backend/src/main/java/bilfo/demo/Triple.java
+++ Backend/src/main/java/bilfo/demo/Triple.java
@@ -1,14 +1,18 @@
 package bilfo.demo;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@EqualsAndHashCode
+@ToString
 public class Triple <T1, T2, T3>{
     private final T1 first;
     private final T2 second;
     private final T3 third;
 
-    public Triple(T1 first, T2 second, T3 third) {
+    private Triple(T1 first, T2 second, T3 third) {
         this.first = first;
         this.second = second;
         this.third = third;
